a130212a5a95243cbd965da70211f2fb0b5c134f
JSphereDev <106980605+JSphereDev@users.noreply.github.com>
2022-06-14 15:53:17 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 export async function message(ctx: any) : Promise<any> {
     debugger;
-    return ctx.response.text('JSphere - Hello World');
+    return ctx.response.text('JSphere - Hello There');
 }
 message.attributes = { method: 'GET' };
